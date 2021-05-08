9dc02687a4d79968b5a7f97ba9cbc823c82dea13
Anthony O'Brien <aux.fuse@yahoo.com>
2021-05-09 00:23:28 +0100

    Register template via register.html created and setup to handle register_form

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
 from os import path
 from flask import Flask, render_template, redirect, url_for, flash, session
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from flask_bcrypt import Bcrypt
+from forms import LoginForm, RegisterForm, CreatePost, EditPost, DeletePost
+
 
 if path.exists("env.py"):
     import env
@@ -8,14 +13,48 @@ if path.exists("env.py"):
 app = Flask(__name__)
 
 app.config.update(
+    MONGO_DB = os.environ.get('MONGO_DB'),
+    MONGO_URI = os.environ.get('MONGO_URI'),
     SECRET_KEY = os.environ.get('SECRET_KEY')
 )
+
+mongo = PyMongo(app)
+users = mongo.db.users
+posts = mongo.db.posts
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    register_form = RegisterForm()
+
+    if register_form.validate_on_submit():
+        found_username = users.find_one({
+            'username': request.form['username']
+        })
+
+        if not found_username:
+            hashed_pw = Bcrypt.generate_password_hash(
+                request.form['password']).decode('utf-8')
+            users.insert_one({
+                'username': register_form.username.data,
+                'password': hashed_pw
+            })
+            session['username'] = request.form.get('username')
+            return redirect(url_for('index'))
+        else:
+            flash(f'Duplicate account detected. Please try again!', 'danger')
+            return redirest(url_for('register'))
+    
+    return render_template(
+        'register.html', title='Register', form=register_form
+        )
+
 
 
 if __name__ == "__main__":
